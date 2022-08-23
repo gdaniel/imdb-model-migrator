2515d0584bd7e48be0a22fb5aaba02c673f70d37
Gwendal Daniel <gwendal.daniel@gmail.com>
2022-08-24 01:49:05 +0200

    extract input/output XMI file locations

--- imdb/src/App.java
+++ imdb/src/App.java
@@ -16,18 +16,27 @@ import movies.MoviesPackage;
 import movies.RootElement;
 
 public class App {
+	
+	/*
+	 * The original file downloaded from https://atenea.lcc.uma.es/projects/LinTra.html
+	 */
+	private static final String INPUT_XMI = "model/movies-test.xmi";
+	
+	/*
+	 * The output file with the adapted model that works with the SparkTE importer.
+	 */
+	private static final String OUTPUT_XMI = "model/output-movies-test.xmi";
 
 	public static void main(String[] args) throws IOException {
 		EPackage.Registry.INSTANCE.put(MoviesPackage.eINSTANCE.getNsURI(), MoviesPackage.eINSTANCE);
 		ResourceSet rSet = new ResourceSetImpl();
 		rSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
-		Resource input = rSet.getResource(URI.createURI("model/movies-test.xmi"), true);
+		Resource input = rSet.getResource(URI.createURI(INPUT_XMI), true);
 		System.out.println(input.getContents().get(0));
 		
-		Resource output = rSet.createResource(URI.createURI("model/output-movies-test.xmi"));
+		Resource output = rSet.createResource(URI.createURI(OUTPUT_XMI));
 		RootElement r = MoviesFactory.eINSTANCE.createRootElement();
 		output.getContents().add(r);
-//		Iterable<EObject> inEObjects = () -> input.getAllContents();
 		List<Element> el = input.getContents().stream().map(e -> (Element)e)
 			.toList();
 		r.getContent().addAll(el);
